bdf9b3e342c6e2b0d07e947511d9d2f258513425
Wendel-Fl <wendelfl.dev@gmail.com>
2022-09-13 18:53:16 -0300

    Testes

--- testPackages/JUnitPJ/TestPJ.java
+++ testPackages/JUnitPJ/TestPJ.java
@@ -4,11 +4,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import dominio.PessoaJuridica;
+import formularios.janelaDeCadastroPJ;
+
 public class TestPJ {
 
+	janelaDeCadastroPJ cadastroPj = new janelaDeCadastroPJ();
+	PessoaJuridica pessoaJuridica = new PessoaJuridica();
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testNome() {		
+		assertTrue(pessoaJuridica.getRazaoSocial().equals(cadastroPj));
+	}
+	
+	@Test
+	public void testEndereco() {
+		assertTrue(pessoaJuridica.getEndereco().equals(cadastroPj));
+		
 	}
 
 }
